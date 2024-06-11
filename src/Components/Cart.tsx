48741968c495057ed807c9c82fc5e087a4034877
dawitMesfin <dawitmesfin2019@gmail.com>
2024-06-11 12:28:07 +0300

    this is the final version

--- src/Components/Cart.tsx
+++ src/Components/Cart.tsx
@@ -7,10 +7,23 @@ interface CartProps {
 }
 
 const Cart = ({ allProducts ,onClear}: CartProps) => {
+
+  const liStyle = {
+    backgroundColor: '#f0f8ff',
+    display : 'flex',
+    justifyContent: 'space-around',
+    margin: '10px 0',
+    padding: '10px',
+    borderRadius: '5px',
+    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+    transition: 'background-color 0.3s',
+    cursor: 'pointer' // Add cursor style for better UX
+  };
   return (
     <>
-    <ul>
-    {allProducts.map(product => <li style = {{margin : '10px'}}key = {product}>{product} <button style = {{background: 'red'}} onClick = {() => onClear(product)}>Clear Item</button></li>)}
+    <ul style={{listStyle: 'none'}}>
+    {allProducts.map(product => <li style = {liStyle} key = {product}>{product}
+     <button  onClick = {() => onClear(product)} style={{background : 'red', borderRadius: '20px'}}>Clear Item</button></li>)}
     </ul>
     </>
   );
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -5,9 +5,22 @@ interface NavBarProps {
 }
 
 const NavBar = ({numberOfProductItems} : NavBarProps) => {
+
+  const circularSpanStyle: React.CSSProperties= {
+    display: 'inline-block',
+    width: '50px',
+    height: '50px',
+    borderRadius: '50%',
+    backgroundColor: '#3498db',
+    textAlign: 'center',
+    lineHeight: '50px',
+    color: 'white',
+    fontSize: '16px',
+  };
+  
   return (
     <div>
-      <h1>You have {numberOfProductItems} in the cart</h1>
+      <h1>You have <span style={circularSpanStyle}>{numberOfProductItems}</span> items in the cart</h1>
     </div>
   );
 };
